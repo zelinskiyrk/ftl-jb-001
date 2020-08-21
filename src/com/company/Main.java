2102a57113afb1232c0f1eda44cab0493c116a3d
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-08-21 12:16:46 +0400

    L005 - methods examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,23 @@
 package com.company;
 
-import com.company.L001.HelloWorld;
-import com.company.L002.POJO;
-import com.company.L003.Cat;
+
+import com.company.animals.Cat;
 
 public class Main {
 
     public static void main(String[] args) {
-        HelloWorld.run();
-        POJO.run();
+//        HelloWorld.run();
+//        POJO.run();
+//
+//
+//        Cat catVar = new Cat();
+//        catVar.voice();
+//
+//        com.company.L004.Cat catL004 = new com.company.L004.Cat();
+//        catL004.voice();
 
-        Cat catVar = new Cat();
-        catVar.voice();
-        
-        com.company.L004.Cat catL004 = new com.company.L004.Cat();
-        catL004.voice();
+        Cat cat = new Cat("Barsik", 20);
+        System.out.println(cat.jumpHeight());
     }
 }
+
