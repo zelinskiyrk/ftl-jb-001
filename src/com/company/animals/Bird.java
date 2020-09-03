1238754f61e99f5f838206339b0be98e70e62aa9
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-03 17:17:09 +0400

    L022 - Abstract class example

--- src/com/company/animals/Bird.java
+++ src/com/company/animals/Bird.java
@@ -1,6 +1,6 @@
 package com.company.animals;
 
-public class Bird extends Animal implements Flight {
+public abstract class Bird extends Animal implements Flight {
     protected Boolean flyModeActive = false;
 
     public Bird() {
