986fe23da8e0f7c830470f125d86728233833b4c
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-04 15:22:25 +0400

    Task007(2)

--- src/com/company/tasks/task007/Task007.java
+++ src/com/company/tasks/task007/Task007.java
@@ -1,2 +1,15 @@
-package com.company.tasks.task007;public class Task007 {
+package com.company.tasks.task007;
+
+public class Task007 {
+    public static void run(){
+        System.out.println("");
+        System.out.println("Task007");
+
+        for(int j = 0; j <3; j++) {
+            for (int i = 0; i < 5; i++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
 }
