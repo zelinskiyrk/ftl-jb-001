dbcc0853c145eeeccc9cdc099e1d9cd87c1767d3
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-04 14:09:31 +0400

    Task001 - Форматирование вывода (Переход на новую строку, в заголовке вывода теперь отображается номер задания)

--- src/com/company/tasks/task001/Task001.java
+++ src/com/company/tasks/task001/Task001.java
@@ -2,8 +2,10 @@ package com.company.tasks.task001;
 
 public class Task001 {
     public static void run(){
+        System.out.println("Task001");
         for(int i = 1; i < 6; i++){
             System.out.print(i + " ");
         }
+        System.out.println("");
     }
 }
