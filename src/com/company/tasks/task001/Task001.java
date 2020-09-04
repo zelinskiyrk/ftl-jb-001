c12dbdbb47d68b8083b6d29a478abe9e4f3eaf14
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-04 12:07:32 +0400

    Task001

---
+++ src/com/company/tasks/task001/Task001.java
@@ -0,0 +1,9 @@
+package com.company.tasks.task001;
+
+public class Task001 {
+    public static void run(){
+        for(int i = 1; i < 6; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
