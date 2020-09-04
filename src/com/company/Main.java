fb332853ae539e81903860aed36e293d21d398ea
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-04 13:09:46 +0400

    Task003

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ import com.company.L028.House;
 import com.company.animals.*;
 import com.company.tasks.task001.Task001;
 import com.company.tasks.task002.Task002;
+import com.company.tasks.task003.Task003;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -30,107 +31,110 @@ public class Main {
 //        com.company.L004.Cat catL004 = new com.company.L004.Cat();
 //        catL004.voice();
 
-        Cat cat = new Cat("Barsik", new Animal.AnimalWeight(20, Animal.AnimalWeight.WeightType.KG));
-        System.out.println(cat.jumpHeight());
-        cat.voice();
+//        Cat cat = new Cat("Barsik", new Animal.AnimalWeight(20, Animal.AnimalWeight.WeightType.KG));
+//        System.out.println(cat.jumpHeight());
+//        cat.voice();
+//
+//        Dog dog = new Dog();
+//
+//        dog.goToStick(14);
+//        dog.whereTheDog();
+//
+//        dog.goToStick(20);
+//        dog.whereTheDog();
+//
+//        dog.goToStick(5);
+//        dog.whereTheDog();
+//
+//        dog.voice();
+//
+//        Dog homelessDog = Dog.ofHomeless(new Animal.AnimalWeight(100, Animal.AnimalWeight.WeightType.KG));
+//        homelessDog.voice();
+//
+//        Duck duck = new Duck();
+//        duck.voice();
+//
+//        duck.takeOff();
+//        duck.isTheBirdFlying();
+//        duck.showSpeed();
+//
+//        duck.landing();
+//        duck.isTheBirdFlying();
+//        duck.showSpeed();
+//
+//        List<Dog> dogs = Dog.randomArray();
+//        for (int i = 0; i < dogs.size(); i++) {
+//            System.out.println(dogs.get(i));
+//        }
+//
+//        List<Dog> dogsHomeless = Arrays.asList(new Dog[]{
+//                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
+//                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
+//                Dog.ofHomeless(new Animal.AnimalWeight(5, Animal.AnimalWeight.WeightType.KG))
+//        });
+//        System.out.println(dogsHomeless.size());
+//        for (Dog dogItem : dogsHomeless) {
+//            System.out.println(dogItem);
+//            dogItem.setName(String.valueOf((new Random()).nextLong()));
+//            System.out.println(dogItem.getName());
+//        }
+//
+//        List<Dog> dogsLinkedList = new LinkedList<>();
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//
+//        int indexLinkedList = 0;
+//        while (indexLinkedList < dogsLinkedList.size()) {
+//            System.out.println(dogsLinkedList.get(indexLinkedList));
+//            indexLinkedList++;
+//        }
+//
+//        for (int i = 0; i < 100; i++) {
+//            //  SwitchExample.run();
+//        }
+//
+//        System.out.println(duck.getMoveType());
+//        System.out.println(dog.getMoveType().getValue());
+//
+//        if (dog.getMoveType() == MoveType.WALK) {
+//            System.out.println("Может ходить");
+//        }
+//
+//        StringExample.run();
+//
+//        TypeConversion.run();
+//
+//        dog.setWeight(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG));
+//        try {
+//            dog.getWeight().setValue(-10);
+//        } catch (Animal.WeightException ignore) {
+//        }
+//
+//        FileExample.run();
+//
+//        //ThreadExample.run();
+//
+//        House house = new House.Builder()
+//                .optHasSwimmingPool(true)
+//                .build();
+//        System.out.println(house.toString());
+//
+//        House houseFull = new House.Builder()
+//                .optHasFancyStatues(true)
+//                .optHasGarden(true)
+//                .optHasGarage(true)
+//                .optHasSwimmingPool(true)
+//                .build();
+//        System.out.println(houseFull.toString());
 
-        Dog dog = new Dog();
-
-        dog.goToStick(14);
-        dog.whereTheDog();
-
-        dog.goToStick(20);
-        dog.whereTheDog();
-
-        dog.goToStick(5);
-        dog.whereTheDog();
-
-        dog.voice();
-
-        Dog homelessDog = Dog.ofHomeless(new Animal.AnimalWeight(100, Animal.AnimalWeight.WeightType.KG));
-        homelessDog.voice();
-
-        Duck duck = new Duck();
-        duck.voice();
-
-        duck.takeOff();
-        duck.isTheBirdFlying();
-        duck.showSpeed();
-
-        duck.landing();
-        duck.isTheBirdFlying();
-        duck.showSpeed();
-
-        List<Dog> dogs = Dog.randomArray();
-        for (int i = 0; i < dogs.size(); i++) {
-            System.out.println(dogs.get(i));
-        }
-
-        List<Dog> dogsHomeless = Arrays.asList(new Dog[]{
-                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
-                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
-                Dog.ofHomeless(new Animal.AnimalWeight(5, Animal.AnimalWeight.WeightType.KG))
-        });
-        System.out.println(dogsHomeless.size());
-        for (Dog dogItem : dogsHomeless) {
-            System.out.println(dogItem);
-            dogItem.setName(String.valueOf((new Random()).nextLong()));
-            System.out.println(dogItem.getName());
-        }
-
-        List<Dog> dogsLinkedList = new LinkedList<>();
-        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
-        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
-        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
-        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
-        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
-
-        int indexLinkedList = 0;
-        while (indexLinkedList < dogsLinkedList.size()) {
-            System.out.println(dogsLinkedList.get(indexLinkedList));
-            indexLinkedList++;
-        }
-
-        for (int i = 0; i < 100; i++) {
-            //  SwitchExample.run();
-        }
-
-        System.out.println(duck.getMoveType());
-        System.out.println(dog.getMoveType().getValue());
-
-        if (dog.getMoveType() == MoveType.WALK) {
-            System.out.println("Может ходить");
-        }
-
-        StringExample.run();
-
-        TypeConversion.run();
-
-        dog.setWeight(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG));
-        try {
-            dog.getWeight().setValue(-10);
-        } catch (Animal.WeightException ignore) {
-        }
-
-        FileExample.run();
-
-        //ThreadExample.run();
-
-        House house = new House.Builder()
-                .optHasSwimmingPool(true)
-                .build();
-        System.out.println(house.toString());
-
-        House houseFull = new House.Builder()
-                .optHasFancyStatues(true)
-                .optHasGarden(true)
-                .optHasGarage(true)
-                .optHasSwimmingPool(true)
-                .build();
-        System.out.println(houseFull.toString());
+        // Практические задания
 
         Task001.run();
         Task002.run();
+        Task003.run();
     }
 }
 
---
+++ src/com/company/tasks/task003/Task003.java
@@ -0,0 +1,9 @@
+package com.company.tasks.task003;
+
+public class Task003 {
+    public static void run(){
+        for(int i = 1; i < 11; i++){
+            System.out.println("3*" + i + "=" + 3*i);
+        }
+    }
+}
