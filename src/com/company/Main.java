ba0c99f39eb775acaf7093f7ae11252fb6936423
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-25 18:49:30 +0400

    Insert Main 3

---
+++ src/com/company/Main.java
@@ -0,0 +1,193 @@
+package com.company;
+
+
+import com.company.L013.SwitchExample;
+import com.company.L019.StringExample;
+import com.company.L020.TypeConversion;
+import com.company.L026.FileExample;
+import com.company.L027.ThreadExample;
+import com.company.L028.House;
+import com.company.animals.*;
+import com.company.tasks.task001.Task001;
+import com.company.tasks.task002.Task002;
+import com.company.tasks.task003.Task003;
+import com.company.tasks.task004.Task004;
+import com.company.tasks.task005.Task005;
+import com.company.tasks.task006.Task006;
+import com.company.tasks.task007.Task007;
+import com.company.tasks.task008.Task008;
+import com.company.tasks.task009.Task009;
+import com.company.tasks.task010.Task010;
+import com.company.tasks.task011.Task011;
+import com.company.tasks.task012.Task012;
+import com.company.tasks.task013.Task013;
+import com.company.tasks.task014.Task014;
+import com.company.tasks.task015.Task015;
+import com.company.tasks.task016.Task016;
+import com.company.tasks.task017.Task017;
+import com.company.tasks.task018.Task018;
+import com.company.tasks.task019.Task019;
+import com.company.tasks.task020.Task020;
+import com.company.tasks.task021.Task021;
+import com.company.tasks.task022.Task022;
+import com.company.tasks.task023.Task023;
+import com.company.tasks.task024.Task024;
+import com.company.tasks.task025.Task025;
+import com.company.tasks.task026.Task026;
+import com.company.tasks.task027.Task027;
+import com.company.tasks.task028.Task028;
+import com.company.tasks.task029.Task029;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        HelloWorld.run();
+//        POJO.run();
+//
+//
+//        Cat catVar = new Cat();
+//        catVar.voice();
+//
+//        com.company.L004.Cat catL004 = new com.company.L004.Cat();
+//        catL004.voice();
+
+//        Cat cat = new Cat("Barsik", new Animal.AnimalWeight(20, Animal.AnimalWeight.WeightType.KG));
+//        System.out.println(cat.jumpHeight());
+//        cat.voice();
+//
+//        Dog dog = new Dog();
+//
+//        dog.goToStick(14);
+//        dog.whereTheDog();
+//
+//        dog.goToStick(20);
+//        dog.whereTheDog();
+//
+//        dog.goToStick(5);
+//        dog.whereTheDog();
+//
+//        dog.voice();
+//
+//        Dog homelessDog = Dog.ofHomeless(new Animal.AnimalWeight(100, Animal.AnimalWeight.WeightType.KG));
+//        homelessDog.voice();
+//
+//        Duck duck = new Duck();
+//        duck.voice();
+//
+//        duck.takeOff();
+//        duck.isTheBirdFlying();
+//        duck.showSpeed();
+//
+//        duck.landing();
+//        duck.isTheBirdFlying();
+//        duck.showSpeed();
+//
+//        List<Dog> dogs = Dog.randomArray();
+//        for (int i = 0; i < dogs.size(); i++) {
+//            System.out.println(dogs.get(i));
+//        }
+//
+//        List<Dog> dogsHomeless = Arrays.asList(new Dog[]{
+//                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
+//                Dog.ofHomeless(new Animal.AnimalWeight(3, Animal.AnimalWeight.WeightType.KG)),
+//                Dog.ofHomeless(new Animal.AnimalWeight(5, Animal.AnimalWeight.WeightType.KG))
+//        });
+//        System.out.println(dogsHomeless.size());
+//        for (Dog dogItem : dogsHomeless) {
+//            System.out.println(dogItem);
+//            dogItem.setName(String.valueOf((new Random()).nextLong()));
+//            System.out.println(dogItem.getName());
+//        }
+//
+//        List<Dog> dogsLinkedList = new LinkedList<>();
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//        dogsLinkedList.add(Dog.ofHomeless(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG)));
+//
+//        int indexLinkedList = 0;
+//        while (indexLinkedList < dogsLinkedList.size()) {
+//            System.out.println(dogsLinkedList.get(indexLinkedList));
+//            indexLinkedList++;
+//        }
+//
+//        for (int i = 0; i < 100; i++) {
+//            //  SwitchExample.run();
+//        }
+//
+//        System.out.println(duck.getMoveType());
+//        System.out.println(dog.getMoveType().getValue());
+//
+//        if (dog.getMoveType() == MoveType.WALK) {
+//            System.out.println("Может ходить");
+//        }
+//
+//        StringExample.run();
+//
+//        TypeConversion.run();
+//
+//        dog.setWeight(new Animal.AnimalWeight(10, Animal.AnimalWeight.WeightType.KG));
+//        try {
+//            dog.getWeight().setValue(-10);
+//        } catch (Animal.WeightException ignore) {
+//        }
+//
+//        FileExample.run();
+//
+//        //ThreadExample.run();
+//
+//        House house = new House.Builder()
+//                .optHasSwimmingPool(true)
+//                .build();
+//        System.out.println(house.toString());
+//
+//        House houseFull = new House.Builder()
+//                .optHasFancyStatues(true)
+//                .optHasGarden(true)
+//                .optHasGarage(true)
+//                .optHasSwimmingPool(true)
+//                .build();
+//        System.out.println(houseFull.toString());
+
+        // Практические задания
+      
+//        Task001.run();
+//        Task002.run();
+//        Task003.run();
+//        Task004.run();
+//        Task005.run();
+//        Task006.run();
+//        Task007.run();
+//        Task008.run();
+//        Task009.run();
+//        Task010.run();
+//        Task011.run();
+//        Task012.run();
+//        Task013.run();
+//        Task014.run();
+//        Task015.run();
+//        Task016.run();
+//        Task017.run();
+//        Task018.run();
+//        Task019.run();
+//        Task020.run();
+//        Task021.run();
+//        Task022.run();
+//        Task023.run();
+//        Task024.run();
+//        Task025.run();
+//        Task026.run();
+//        Task027.run();
+//        Task028.run();
+        Task029.run();
+        
+    }
+}
+
