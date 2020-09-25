6b9f9acb0d4ac608846da3e67cf2f8da1c7957d0
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-09-25 19:35:10 +0400

    Task030 #2 (Исправленное)

--- src/com/company/tasks/task030/Task030.java
+++ src/com/company/tasks/task030/Task030.java
@@ -1,4 +1,36 @@
 package com.company.tasks.task030;
 
+import java.util.Arrays;
+
 public class Task030 {
+
+        private static void threadInfo(Thread thread) {
+            System.out.println("Thread: " + thread.getName());
+            System.out.println("Id: " + thread.getId());
+            System.out.println("Priority: " + thread.getPriority());
+            System.out.println("State: " + thread.getState());
+            System.out.println("Thread Group: " + thread.getThreadGroup());
+        }
+
+        public static void run() {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    threadInfo(Thread.currentThread());
+                }
+            });
+
+            System.out.println("START THREAD:");
+            threadInfo(thread);
+            thread.start();
+            System.out.println("FINISH THREAD:");
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadInfo(thread);
+
+        }
 }
+
